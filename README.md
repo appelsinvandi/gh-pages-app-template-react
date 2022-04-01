889ccd6f9c993c7f637d7e378fa444f7e0545231
Morten Jelle <1551620+appelsinvandi@users.noreply.github.com>
2022-04-01 13:02:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-gh-pages-app-template
+
+A template repo for making small react apps hosted on GH Pages.
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+import styles from './App.module.scss'
+
+export type AppProps = {}
+export const App: React.VFC<AppProps> = () => {
+  return <div className={styles.root}>Some cool project</div>
+}
