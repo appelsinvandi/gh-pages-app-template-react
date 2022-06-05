0348b1643669464f99b31de26bda355e2a589974
Morten Jelle <1551620+appelsinvandi@users.noreply.github.com>
2022-06-06 01:57:55 +0200

    add: upgrade to react 18 and add import sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import styles from './App.module.scss'
+import * as styles from './App.module.scss'
 
 export type AppProps = {}
-export const App: React.VFC<AppProps> = () => {
+export const App: React.FC<AppProps> = () => {
   return <div className={styles.root}>Some cool React project</div>
 }
