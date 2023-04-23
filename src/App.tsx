a16a9a3cdb591a4d828a166b5c00f54463b7f972
Morten Jelle <1551620+appelsinvandi@users.noreply.github.com>
2023-04-23 17:24:14 +0200

    fix: tsconfig paths support

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import styles from './App.module.scss'
 
 export type AppProps = {}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+import reactPlugin from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
-import react from '@vitejs/plugin-react'
+import tsconfigPathsPlugin from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactPlugin(), tsconfigPathsPlugin()],
 })
